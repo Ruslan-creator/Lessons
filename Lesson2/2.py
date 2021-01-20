bdac5fc5ed4c888a759130d2a8c408ebfc9b7fa5
Ruslan-creator <77435438+Ruslan-creator@users.noreply.github.com>
2021-01-20 12:16:33 +0300

    Update 2.py

--- Lesson2/2.py
+++ Lesson2/2.py
@@ -2,3 +2,4 @@ numbers = input('Enter your numbers: ').split()
 for el in range(0, len(numbers) - 1, 2):
     numbers[el], numbers[el + 1] = numbers[el + 1], numbers[el]
 print(numbers)
+# не знаю как вывести исходный список(
